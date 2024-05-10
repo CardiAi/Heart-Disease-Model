368b1db89fa027feba747840bb16f9c4f1230d36
youssef-yasser-ali <yyasser849@gmail.com>
2024-05-10 04:14:53 +0300

    Handel Null values

--- model.py
+++ model.py
@@ -6,9 +6,15 @@ import pandas as pd
 model = joblib.load('heart_disease_pipline.pkl')
 
 
+def handle_none_values(data):
+    for key in data:
+        if data[key] is None:
+            data[key] = np.nan
+    return data
 
 
 def model_predict(data):
+    data = handle_none_values(data)
     print(data)
     input_df = pd.DataFrame(data, index=[0])
     prediction = model.predict(input_df)
